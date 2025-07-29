81330f8c4e7797fc7446616061ec3e76a73dbc63
hellomehf <klok8670@gmail.com>
2025-07-29 14:22:36 +0700

    Create Product.php

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name', 'description', 'price', 'sale_price', 'category_id',
+        'expire_date', 'featured', 'stock_status', 'quantity'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+}
