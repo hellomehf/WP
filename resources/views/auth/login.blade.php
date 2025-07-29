ced2f500e6aeb9dc2d56b355567240783bf4d2b1
hellomehf <klok8670@gmail.com>
2025-07-29 14:22:52 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,20 +42,30 @@
         <div class="tab-pane fade show active" id="tab-item-login" role="tabpanel" aria-labelledby="login-tab">
           <div class="login-form">
 
-            <form method="post" action="{{route('login')}}" class="needs-validation" novalidate="">
-              @csrf
+            <form method="POST" action="{{route('login')}}" class="needs-validation" novalidate="">
+                @csrf
               <div class="form-floating mb-3">
-                <input class="form-control form-control_gray " id='email' name='email' value="" required="" autocomplete="email"
+                <input class="form-control form-control_gray @error('email') is-invalid @enderror" id='email' name='email' value="{{ old('email') }}" required="" autocomplete="email"
                   autofocus="">
                 <label for="email">Email address *</label>
+                @error('email')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+                @enderror
               </div>
-
+              
               <div class="pb-3"></div>
-
+              
               <div class="form-floating mb-3">
-                <input id='password' type='password' class="form-control form-control_gray " name="password" required=""
-                  autocomplete="current-password">
+                <input id='password' type='password' class="form-control form-control_gray @error('password') is-invalid @enderror" name="password" required=""
+                autocomplete="current-password">
                 <label for="customerPasswodInput">Password *</label>
+                @error('password')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+                @enderror
               </div>
 
               <button class="btn btn-primary w-100 text-uppercase" type="submit">Log In</button>
