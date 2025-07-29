834c422e2567d875072c5ac85114d942f338afbd
hellomehf <klok8670@gmail.com>
2025-07-29 14:23:04 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,79 +1,47 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use App\Http\Middleware\UserAuthication;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Public routes (accessible to everyone)
-// The root route now points to the login form
+// Authentication Routes
 Route::get('/', [UserController::class, 'LoginForm'])->name('login.form');
 
-// Login routes
-Route::get('login', [UserController::class, 'LoginForm'])->name('login.form');
-Route::post('login', [UserController::class, 'login'])->name('login');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Logout route
-Route::post('logout', [UserController::class, 'logout'])->name('logout'); // Changed to POST for security
+// Protected Routes
+Route::middleware(['auth'])->group(function () {
+    // Admin Dashboard
 
-// Explicit registration route (if you want a separate /register URL)
-Route::get('/register', [UserController::class, 'registerForm'])->name('register');
-
-// Protected routes (require authentication via UserAuthication middleware)
-Route::middleware([UserAuthication::class])->group(function () {
-
-    Route::get('/admin/dashboard', function () {
-        return view('admin.dashboard'); 
+    Route::get('/' , function(){
+        return view('admin.dashboard');
     })->name('admin.dashboard');
+    
+    // Products
+    Route::resource('products', ProductController::class);
+    Route::get('/showProduct' , [ProductController::class, 'show'])->name('pages.product');
+    Route::get('/editProduct' , [ProductController::class, 'edit'])->name('edit.product');
+    Route::get('/addProduct' , [ProductController::class, 'create'])->name('add.product');
 
-    Route::get('/product' , function(){
-        return view('pages.product');
-    })->name('pages.product');
+    // Categories
+    Route::resource('categories', CategoryController::class);
+    Route::get('/showCategory' , [CategoryController::class, 'show'])->name('pages.category');
+    Route::get('/editCategory' , [CategoryController::class, 'edit'])->name('edit.category');
+    Route::get('/addCategory' , [CategoryController::class, 'create'])->name('pages.addcategory');
 
-    Route::get('/addproduct' , function() {
-        return view('pages.addproduct');
-    })->name('add.product');
+    // Users
+    Route::resource('users', UserController::class)->except(['create', 'store']);
+    Route::get('/showUser' , [UserController::class, 'show'])->name('pages.user');
+    Route::get('/showUserPf' , [UserController::class, 'showPf'])->name('pages.userpf');
+    Route::get('/addUser' , [UserController::class , 'create'])->name('add.user');
+    Route::get('/editUser' , [UserController::class , 'edit'])->name('pages.edituser');
 
-    Route::get('/editProduct' , function() {
-        return view('pages.editproduct');
-    })->name('edit.product');
-
-    Route::get('/category' , function(){
-        return view('pages.category');
-    })->name('pages.category');
-
-    Route::get('/addcategory' , function(){
-        return view('pages.addcategory');
-    })->name('pages.addcategory');
-
-    Route::get('/editcategory' , function(){
-        return view('pages.editcategory');
-    })->name('edit.category');
-
-    Route::get('/user' , function(){
-        return view('pages.user');
-    })->name('pages.user');
-
-    Route::get('/userProfile' , function(){
-        return view('pages.userpf');
-    })->name('pages.userpf');
-
-    Route::get('/editUser' , function(){
-        return view('pages.edituser');
-    })->name('pages.edituser');
-
-    Route::get('/addUser' , function(){
-        return view('pages.adduser');
-    })->name('add.user');
-
+    
 });
+
