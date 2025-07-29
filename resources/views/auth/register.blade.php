a41d1df920ec67cf4793322b1d9eb41ab257c897
hellomehf <klok8670@gmail.com>
2025-07-29 14:22:55 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,25 +41,49 @@
       <div class="tab-content pt-2" id="login_register_tab_content">
         <div class="tab-pane fade show active" id="tab-item-register" role="tabpanel" aria-labelledby="register-tab">
           <div class="register-form">
-            <form method="POST" action="#" name="register-form" class="needs-validation" novalidate="">
+            <form method="POST" action="{{ route('register') }}" name="register-form" class="needs-validation" novalidate="">
+                @csrf
               <div class="form-floating mb-3">
-                <input class="form-control form-control_gray " name="name" value="" required="" autocomplete="name"
+                <input class="form-control form-control_gray @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required="" autocomplete="name"
                   autofocus="">
                 <label for="name">Name</label>
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
               </div>
+
               <div class="pb-3"></div>
+
               <div class="form-floating mb-3">
-                <input id="email" type="email" class="form-control form-control_gray " name="email" value="" required=""
+                <input id="email" type="email" class="form-control form-control_gray @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required=""
                   autocomplete="email">
                 <label for="email">Email address *</label>
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
               </div>
 
               <div class="pb-3"></div>
 
               <div class="form-floating mb-3">
-                <input id="password" type="password" class="form-control form-control_gray " name="password" required=""
+                <input id="password" type="password" class="form-control form-control_gray @error('password') is-invalid @enderror" name="password" required=""
                   autocomplete="new-password">
                 <label for="password">Password *</label>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              </div>
+
+              <div class="form-floating mb-3">
+                <input id="password-confirm" type="password" class="form-control form-control_gray "
+                  name="password_confirmation" required="" autocomplete="new-password">
+                <label for="password">Confirm Password *</label>
               </div>
 
               <div class="d-flex align-items-center mb-3 pb-2">
@@ -71,7 +95,7 @@
 
               <div class="customer-option mt-4 text-center">
                 <span class="text-secondary">Have an account?</span>
-                <a href="#" class="btn-text js-show-register">Login to your Account</a>
+                <a href="{{route('login')}}" class="btn-text js-show-register">Login to your Account</a>
               </div>
             </form>
           </div>
