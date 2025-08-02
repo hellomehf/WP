6f1baa00362e4853bbce9ee78f3de56fb9d16669
hellomehf <klok8670@gmail.com>
2025-08-02 16:01:09 +0700

    Update user.blade.php

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -22,19 +22,9 @@
         <div class="wg-box">
             <div class="flex items-center justify-between gap10 flex-wrap">
                 <div class="wg-filter flex-grow">
-                    <form class="form-search">
-                        <fieldset class="name">
-                            <input type="text" placeholder="Search here..." class="" name="name"
-                                tabindex="2" value="" aria-required="true" required="">
-                        </fieldset>
-                        <div class="button-submit">
-                            <button class="" type="submit"><i class="icon-search"></i></button>
-                        </div>
-                    </form>
+
                 </div>
-                <form action="#">
-                    <a class="tf-button style-1 w208" href="{{route('add.user')}}"><i class="icon-plus"></i>Add new User</a>
-                </form>
+
             </div>
             <div class="wg-table table-all-user">
 
@@ -42,43 +32,24 @@
                     <table class="table table-striped table-bordered">
                         <thead>
                             <tr>
-                              <th>#</th>
+                              <th style="width: 80px" class="text-center">#</th>
                               <th>User</th>
-                              <th>Phone</th>
                               <th>Email</th>
-                              <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
                             {{-- for getdata from database --}}
-                          <tr>
-                              <td>1</td>
-                              <td class="pname">
-                                  <div class="image">
-                                      <img src="{{asset('images\avatar\user-1.png')}}" alt="" class="image">
-                                  </div>
-                                  <div class="name">
-                                      <a href="{{route('pages.userpf')}}" class="body-title-2">Admin</a>
-                                      <div class="text-tiny mt-3">ADM</div>
-                                  </div>
-                              </td>
-                              <td>1234567890</td>
-                              <td>admin@surfsidemedia.in</td>
-                              <td>
-                                  <div class="list-icon-function">
-                                      <a href="{{route('pages.edituser')}}">
-                                          <div class="item edit">
-                                              <i class="icon-edit-3"></i>
-                                          </div>
-                                      </a>
-                                      <form action="#" method="POST">
-                                        <div class="item text-danger delete">
-                                            <i class="icon-trash-2"></i>
-                                        </div>
-                                      </form>
-                                  </div>
-                              </td>
-                          </tr>
+                            @foreach ($users as $user)
+                            <tr>
+                                <td class="text-center">{{$user->id}}</td>
+                                <td class="pname">
+                                    <div class="name">
+                                        <div class="body-title-3 mt-3">{{$user->name}}</div>
+                                    </div>
+                                </td>
+                                <td>{{$user->email}}</td>
+                            </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
