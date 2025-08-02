5944e8cedbe42d3f5d8bc5626a5175ddb7f9c5d7
hellomehf <klok8670@gmail.com>
2025-08-02 16:00:18 +0700

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,23 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = [
-        'name', 'description', 'price', 'sale_price', 'category_id',
-        'expire_date', 'featured', 'stock_status', 'quantity'
-    ];
+    use HasFactory;
 
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class,'category_id');
     }
-
-    public function images()
-    {
-        return $this->hasMany(ProductImage::class);
-    }
-
 }
