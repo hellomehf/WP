3530b2da42c5a46653030f8c92b1511e1ba04af7
hellomehf <klok8670@gmail.com>
2025-08-02 16:01:33 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,10 +7,9 @@
 @section('content')
     <div class="main-content-inner">
         <div class="main-content-wrap">
-            <div class="tf-section-2 mb-30">
+            <div class="tf-section-1 mb-30">
                 <div class="flex gap20 flex-wrap-mobile">
                     <div class="w-half">
-
                         <div class="wg-chart-default mb-20">
                             <div class="flex items-center justify-between">
                                 <div class="flex items-center gap14">
@@ -18,62 +17,14 @@
                                         <i class="icon-shopping-bag"></i>
                                     </div>
                                     <div>
-                                        <div class="body-text mb-2">Total Orders</div>
-                                        <h4>3</h4>
+                                        <div class="body-text mb-2">Total Product</div>
+                                        <h4>{{$dashProduct[0]->totalproduct}}</h4>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-
-                        <div class="wg-chart-default mb-20">
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center gap14">
-                                    <div class="image ic-bg">
-                                        <i class="icon-dollar-sign"></i>
-                                    </div>
-                                    <div>
-                                        <div class="body-text mb-2">Total Amount</div>
-                                        <h4>481.34</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="wg-chart-default mb-20">
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center gap14">
-                                    <div class="image ic-bg">
-                                        <i class="icon-shopping-bag"></i>
-                                    </div>
-                                    <div>
-                                        <div class="body-text mb-2">Pending Orders</div>
-                                        <h4>3</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="wg-chart-default">
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center gap14">
-                                    <div class="image ic-bg">
-                                        <i class="icon-dollar-sign"></i>
-                                    </div>
-                                    <div>
-                                        <div class="body-text mb-2">Pending Orders Amount</div>
-                                        <h4>481.34</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
-
                     <div class="w-half">
-
                         <div class="wg-chart-default mb-20">
                             <div class="flex items-center justify-between">
                                 <div class="flex items-center gap14">
@@ -81,74 +32,44 @@
                                         <i class="icon-shopping-bag"></i>
                                     </div>
                                     <div>
-                                        <div class="body-text mb-2">Delivered Orders</div>
-                                        <h4>0</h4>
+                                        <div class="body-text mb-2">Total Category</div>
+                                        <h4>{{$dashCategory[0]->totalcategory}}</h4>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-
-                        <div class="wg-chart-default mb-20">
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center gap14">
-                                    <div class="image ic-bg">
-                                        <i class="icon-dollar-sign"></i>
-                                    </div>
-                                    <div>
-                                        <div class="body-text mb-2">Delivered Orders Amount</div>
-                                        <h4>0.00</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="wg-chart-default mb-20">
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center gap14">
-                                    <div class="image ic-bg">
-                                        <i class="icon-shopping-bag"></i>
-                                    </div>
-                                    <div>
-                                        <div class="body-text mb-2">Canceled Orders</div>
-                                        <h4>0</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="wg-chart-default">
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-center gap14">
-                                    <div class="image ic-bg">
-                                        <i class="icon-dollar-sign"></i>
-                                    </div>
-                                    <div>
-                                        <div class="body-text mb-2">Canceled Orders Amount</div>
-                                        <h4>0.00</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
+                    <div class="w-half">
+                        <div class="wg-chart-default mb-20">
+                            <div class="flex items-center justify-between">
+                                <div class="flex items-center gap14">
+                                    <div class="image ic-bg">
+                                        <i class="icon-shopping-bag"></i>
+                                    </div>
+                                    <div>
+                                        <div class="body-text mb-2">Total User</div>
+                                        <h4>{{$dashUser[0]->totaluser}}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+
 
                 </div>
 
                 
 
-            </div>
+            </div> 
             <div class="tf-section mb-30">
 
                 <div class="wg-box">
                     <div class="flex items-center justify-between">
                         <h5>Recent Product Add</h5>
                         <div class="dropdown default">
-                            <a class="btn btn-secondary dropdown-toggle" href="#">
-                                <span class="view-all">View all</span>
-                            </a>
+                           
                         </div>
                     </div>
                     <div class="wg-table table-all-user">
@@ -156,42 +77,56 @@
                             <table class="table table-striped table-bordered">
                                 <thead>
                                     <tr>
-                                        <th style="width: 80px">#</th>
+                                        <th>#</th>
                                         <th>Name</th>
-                                        <th class="text-center">Price</th>
-                                        <th class="text-center">SalePrice</th>
-                                        <th class="text-center">Expire Date</th>
-                                        <th class="text-center">Category</th>
-                                        <th class="text-center">Stauts</th>
-                                        <th class="text-center">Stock</th>
-                                        <th class="text-center">Quantity</th>
-                                        <th class="text-center">Action</th>
-                                        <th></th>
+                                        <th>Price</th>
+                                        <th>SalePrice</th>
+                                        <th>Expire Month</th>
+                                        <th>Category</th>
+                                        <th>Featured</th>
+                                        <th>Stock</th>
+                                        <th>Quantity</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td class="text-center">1</td>
-                                        <td class="text-center">Divyansh Kumar</td>
-                                        <td class="text-center">1234567891</td>
-                                        <td class="text-center">$172.00</td>
-                                        <td class="text-center">$36.12</td>
-                                        <td class="text-center">$208.12</td>
-
-                                        <td class="text-center">ordered</td>
-                                        <td class="text-center">2024-07-11 00:54:14</td>
-                                        <td class="text-center">2</td>
-                                        <td></td>
-                                        <td class="text-center">
-                                            <a href="{{route('edit.product')}}">
-                                                <div class="list-icon-function view-icon">
-                                                    <div class="item eye">
-                                                        <i class="icon-eye"></i>
-                                                    </div>
+                                @foreach ($products as $product)
+                                <tr>
+                                    <td>{{$product->id}}</td>
+                                    <td class="pname">
+                                        <div class="image">
+                                            <img src="{{asset('uploads/products')}}/{{$product->image}}" alt="{{$product->name}}" class="image">
+                                        </div>
+                                        <div class="name">
+                                            <a href="#" class="body-title-2">{{$product->name}}</a>
+                                            <div class="text-tiny mt-3">{{$product->slug}}</div>
+                                        </div>  
+                                    </td>
+                                    <td>${{$product->regular_price}}</td>
+                                    <td>${{$product->sale_price}}</td>
+                                    <td>{{$product->expire_month}}</td>
+                                    <td>{{$product->category->name}}</td>
+                                    <td>{{$product->feature == 0 ? "No":"Yes"}}</td>
+                                    <td>{{$product->stock_status}}</td>
+                                    <td>{{$product->quantity}}</td>
+                                    <td>
+                                        <div class="list-icon-function">
+                                            <a href="{{route('admin.edit.product',['id'=>$product->id])}}">
+                                                <div class="item edit">
+                                                    <i class="icon-edit-3"></i>
                                                 </div>
                                             </a>
-                                        </td>
-                                    </tr>
+                                            <form action="{{route('delete.product',['id'=>$product->id])}}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <div class="item text-danger delete">
+                                                    <i class="icon-trash-2"></i>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
